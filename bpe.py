48922990c13baf0e93b0bd52613cfe3700d89679
Sasank <213149805+sash070@users.noreply.github.com>
2026-07-12 17:06:25 +0530

    Refactor merge function in BPETokenizer to efficiently update pair counts and track scan iterations during training.

--- bpe.py
+++ bpe.py
@@ -26,11 +26,11 @@ def pair_counts(byte_chunks):
             pairs[(token_a, token_b)] = pairs.get((token_a, token_b), 0)+1
     return pairs
 
-encode_rules = []
-decode_dict = {i : (i,) for i in range(256)}
 
-def merge(byte_chunks, merge_id, encode_rules, decode_dict):
-    pairs = pair_counts(byte_chunks)
+def merge(byte_chunks, merge_id, encode_rules, decode_dict, pairs, counter=0):
+    if len(pairs) == 0:
+        pairs |= pair_counts(byte_chunks)
+        counter += 1
     max_pair = None
     max_count = 0
     for pair, count in pairs.items():
@@ -42,12 +42,22 @@ def merge(byte_chunks, merge_id, encode_rules, decode_dict):
             for i in range(len(byte_chunk)-2, -1, -1):
                 token_a, token_b = byte_chunk[i], byte_chunk[i+1]
                 if token_a == max_pair[0] and token_b == max_pair[1]:
+                    if i > 0:
+                        pairs[(byte_chunk[i-1], byte_chunk[i])] -= 1
+                    if i < len(byte_chunk)-2:
+                        pairs[(byte_chunk[i+1], byte_chunk[i+2])] -= 1
                     byte_chunk[i] = merge_id
                     byte_chunk.pop(i+1)
+                    pairs[(token_a, token_b)] -= 1
+                    if i < len(byte_chunk)-1:
+                        pairs[(merge_id, byte_chunk[i+1])] = pairs.get((merge_id, byte_chunk[i+1]), 0)+1
+                    if i > 0:
+                        pairs[(byte_chunk[i-1], merge_id)] = pairs.get((byte_chunk[i-1], merge_id), 0)+1
+
         encode_rules.append((max_pair[0], max_pair[1], merge_id))
         decode_dict[merge_id] = decode_dict[max_pair[0]]+decode_dict[max_pair[1]]
-        return True
-    return False
+        return True, counter
+    return False, counter
     
 class BPETokenizer():
     def __init__(self, vocab_size):
@@ -55,16 +65,21 @@ class BPETokenizer():
         self.encode_rules = []
         self.decode_dict = dict()
         self.default_dict = {i:(i,) for i in range(256)}
+        self.pairs = dict()
 
     def train(self, text):
+        counter = 0
         self.encode_rules.clear()
         self.decode_dict = self.default_dict.copy()
+        self.pairs.clear()
         byte_chunks = preprocess(text)
         for i in range(self.vocab_size-256):
             merge_id = 256+i
-            merged = merge(byte_chunks, merge_id, self.encode_rules, self.decode_dict)
+            merged, count = merge(byte_chunks, merge_id, self.encode_rules, self.decode_dict, self.pairs)
+            counter += count
             if merged == False:
                 break
+        print(f"Number of byte-pair scans over entire data = {counter}")
 
     def _encode_chunks(self, text):
         enc_byte_chunks = preprocess(text)
@@ -107,9 +122,9 @@ class BPETokenizer():
 #         text += line
 
 # import time
-# tokenizer = BPETokenizer(vocab_size=300)
+# tokenizer = BPETokenizer(vocab_size=1000)
 # start = time.time()
 # tokenizer.train(text)
 # print(time.time()-start)
 # print(tokenizer.decode(tokenizer.encode("HELLo anAkin!!1")))
-# Executed in 8.51 sec
+# # Executed in 24.22 sec
